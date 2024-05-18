274f7ec1f3ea20ce1b84225a1ba0121e2bce3fe2
ArazureX <bobryk3000@gmail.com>
2024-05-18 09:57:32 +0000

    modified:   dags/process_sales.py

--- dags/process_sales.py
+++ dags/process_sales.py
@@ -1,13 +1,9 @@
 import json
 import os
-import time
-import requests
 from datetime import datetime, timedelta
-from typing import Iterable
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-
+from airflow.providers.http.hooks.http import HttpHook
 
 BASE_DIR = "/workspaces/hm2/Files/"
 
@@ -21,19 +17,22 @@ def run_job1(date, **kwargs):
     raw_data_path = kwargs.get('raw_data_path')
     raw_folder_name = date.strftime('%Y-%m-%d')
     raw_folder_path = os.path.join(raw_data_path, raw_folder_name)
-    date_str=date.strftime('%Y-%m-%d')
+    date_str = date.strftime('%Y-%m-%d')
     print("Starting job1:")
-    resp = requests.post(
-        url=f'http://172.22.0.8:{JOB1_PORT}/fetch_api_data',
-        json={
+    
+    http_hook = HttpHook(method='POST', http_conn_id='job1_api')
+    response = http_hook.run(
+        endpoint='/fetch_api_data',
+        data=json.dumps({
             "date": date_str,
             "raw_dir": raw_folder_path
-        }
+        }),
+        headers={'Content-Type': 'application/json'}
     )
-    assert resp.status_code == 201
+    
+    assert response.status_code == 201
     print("job1 completed!")
   
-
 def run_job2(date, **kwargs):
     raw_data_path = kwargs.get('raw_data_path')
     stg_data_path = kwargs.get('stg_data_path')
@@ -41,56 +40,52 @@ def run_job2(date, **kwargs):
     raw_folder_path = os.path.join(raw_data_path, raw_folder_name)
     stg_folder_name = date.strftime('%Y-%m-%d')
     stg_folder_path = os.path.join(stg_data_path, stg_folder_name)
+    
     print("Starting job2:")
-    resp = requests.post(
-        url=f'http://172.22.0.8:{JOB2_PORT}/convert',
-        
-        json={
+    
+    http_hook = HttpHook(method='POST', http_conn_id='job2_api')
+    response = http_hook.run(
+        endpoint='/convert',
+        data=json.dumps({
             "raw_dir": raw_folder_path,
             "stg_dir": stg_folder_path
-        }
+        }),
+        headers={'Content-Type': 'application/json'}
     )
-    assert resp.status_code == 201
+    
+    assert response.status_code == 201
     print("job2 completed!")
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2022, 8, 11),  
+    'start_date': datetime(2022, 8, 11),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1
 }
 
-start_date = default_args['start_date']
-days_to_process = 3
-processed_days = 0
+date_str = default_args['start_date']
 
 with DAG(
-        dag_id="process_sales",
-        default_args=default_args,
-        schedule = "0 1 * * *",
-        tags=['HM_3'],
-        catchup=True
+    dag_id="process_sales",
+    default_args=default_args,
+    schedule_interval="0 1 * * *",
+    tags=['HM_3'],
+    catchup=True
 ) as dag:
-    while processed_days < days_to_process:
-        date_to_process = start_date - timedelta(days=processed_days)
-        date_to_process_str = date_to_process.strftime('%Y-%m-%d')
-        extract_data_from_api = PythonOperator(
-            task_id=f'extract_data_for_date_{date_to_process_str}_iter_{processed_days}',
-            python_callable=run_job1,
-            op_kwargs={'date': date_to_process, 'raw_data_path': RAW_DIR},
-            provide_context=True
+    extract_data_from_api = PythonOperator(
+        task_id='extract_data_from_api',
+        python_callable=run_job1,
+        op_kwargs={'date': date_str, 'raw_data_path': RAW_DIR},
+        provide_context=True
     )
-   
-        convert_to_avro = PythonOperator(
-            task_id=f'convert_data_for_date_{date_to_process_str}_iter_{processed_days}',
-            python_callable=run_job2,
-            op_kwargs={'date': date_to_process, 'raw_data_path': RAW_DIR, 'stg_data_path': STG_DIR},
-            provide_context=True
+
+    convert_to_avro = PythonOperator(
+        task_id='convert_to_avro',
+        python_callable=run_job2,
+        op_kwargs={'date': date_str, 'raw_data_path': RAW_DIR, 'stg_data_path': STG_DIR},
+        provide_context=True
     )
-        processed_days += 1
-        extract_data_from_api >> convert_to_avro
-       
 
-
+    extract_data_from_api >> convert_to_avro
